0dad1dfdf394a93d95343686f6366b55e7d56b57
basic-app <dev@basic-app.com>
2020-08-26 11:09:22 +0300

    n/a

---
+++ BaseCommand.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author Basic App Dev Team <dev@basic-app.com>
+ * @license MIT
+ * @link https://basic-app.com
+ */
+namespace BasicApp\Command;
+
+use Psr\Log\LoggerInterface;
+use CodeIgniter\CLI\Commands;
+
+abstract class BaseCommand extends \CodeIgniter\CLI\BaseCommand
+{
+
+    protected $autoFlush = true;
+
+    /**
+     * Command constructor.
+     *
+     * @param \Psr\Log\LoggerInterface       $logger
+     * @param \CodeIgniter\CLI\CommandRunner $commands
+     */
+    public function __construct(LoggerInterface $logger, Commands $commands)
+    {
+        parent::__construct($logger, $commands);
+
+        if ($this->autoFlush)
+        {
+            while (ob_get_level() > 0)
+            {
+                ob_end_flush();
+            }
+        }
+    }
+
+}
